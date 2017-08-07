b1413fbc90a4ed0dbc0f73cec7a45b3b09f1d690
Paraskevas Apostolopoulos <papost@fb.com>
2017-08-07 09:22:58 -0700

    commit

--- checkio/home.js
+++ checkio/home.js
@@ -874,9 +874,50 @@ function commonWords(str1, str2) {
   return result.sort().join(',');
 }
 
+function commonWords(str1, str2) {
+  let str1Arr = str1.split(',');
+  let str2Arr = str2.split(',');
+  let result = [];
+  for (let value of str1Arr){
+    if (str2Arr.includes(value)) result.push(value);
+  }
+  return result.sort().join(',');
+}
+
+
 //solution from the internet
 function commonWords(first, second) {
     return first.split(","). filter(x => second.split(",").includes(x)).sort().join(",");
 }
 
-//see if you can do this with filter
+
+/*20170807
+ABSOLUTE SORTING https://js.checkio.org/mission/absolute-sorting/
+Let's try some sorting. Here is an array with the specific rules.
+
+The array has various numbers.
+You should sort it, but sort it by absolute value in ascending order.
+For example, the sequence (-20, -5, 10, 15) will be sorted like so: (-5, 10, 15, -20).
+Your function should return the sorted list .
+
+Precondition: The numbers in the array are unique by their absolute values.
+
+Input: An array of numbers .
+
+Output: The list or tuple (but not a generator) sorted by absolute values in ascending order.
+
+Addition: The results of your function will be shown as a list in the tests explanation panel.
+
+Example:
+
+
+Note from Paris: These examples are typed like the arrays are for Java and not Javascript.
+Instead of absoluteSorting((-20, -5, 10, 15)) it should be absoluteSorting([-20, -5, 10, 15])
+absoluteSorting((-20, -5, 10, 15)) == [-5, 10, 15, -20] # or (-5, 10, 15, -20)
+absoluteSorting((1, 2, 3, 0)) == [0, 1, 2, 3]
+absoluteSorting((-1, -2, -3, 0)) == [0, -1, -2, -3]
+*/
+
+function absoluteSorting(array) {
+  return array.sort((a, b) =>  Math.abs(a) - Math.abs(b));
+}
