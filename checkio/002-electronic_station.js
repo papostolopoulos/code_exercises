8139b72730d83b9bdff9261ebf86961761d5142f
Paraskevas Apostolopoulos <papost@fb.com>
2017-09-07 16:37:09 -0700

    commit

--- checkio/002-electronic_station.js
+++ checkio/002-electronic_station.js
@@ -314,3 +314,140 @@ else return the result
 return Object.keys(newPlants).length === 0 ? result : powerSupply(newNetwork, newPlants);
 
 }
+
+
+/*20170906
+COUNTING TILES https://js.checkio.org/mission/counting-tiles/
+Stephan needs some help building a circular landing zone using the ice square tiles for their new Ice Base.
+Before he converts the area to a construction place,
+Stephan needs to figure out how many square tiles he will need.
+
+Each square tile has size of 1x1 meters.
+You need to calculate how many whole and partial tiles are needed for a circle
+with a radius of N meters.
+The center of the circle will be at the intersection of four tiles.
+For example: a circle with a radius of 2 metres requires 4 whole tiles and 12 partial tiles.
+
+
+
+Input: The radius of a circle as a float
+
+Output: The quantities whole and partial tiles as a list with two integers -- [solid, partial].
+
+Example:
+countingTiles(2) == [4, 12]
+countingTiles(3) == [16, 20]
+countingTiles(2.1) == [4, 20]
+countingTiles(2.5) == [12, 20]
+
+*/
+
+function countingTiles(r) {
+  //Α = π * (r * r)
+  let circleDiameter = r * 2;
+  let circleArea = Math.PI * Math.pow(r, 2);
+  let squaresTotalArea = Math.pow(circleDiameter, 2);
+  console.log('circleDiameter:', circleDiameter);
+  console.log('circleArea:', circleArea);
+  console.log('squaresTotalArea is the total amount of tiles which is ', squaresTotalArea);
+
+}
+
+
+/*20170906
+CUT SENTENCE - https://js.checkio.org/mission/cut-sentence/
+You should cut a given sentence in a way so it become shorter than a given
+length and the cutted version should contains only full words.
+
+If inital sentence is short enough you don't need to put '...' in the end.
+
+Input: Two arguments. First is a sentence as String, and the second -
+how long it can be, as int.
+
+Output: Cuted sentence, as String.
+
+Example:
+
+cutSentence("Hi my name is Alex", 8) == "Hi my..."
+cutSentence("Hi my name is Alex", 4) == "Hi..."
+cutSentence("Hi my name is Alex", 20) == "Hi my name is Alex"
+*/
+
+function cutSentence(str, sentenceLen) {
+  if (str.length <= sentenceLen) return str;
+  var testArr = str.split(' ');
+  let result = '';
+  let oldResult = '';
+  for (let i = 0; i < testArr.length; i++) {
+    oldResult += testArr[i] + ' ';
+    if (oldResult.slice(0, oldResult.length - 1).length <= sentenceLen) result = oldResult;
+    if (oldResult.length > sentenceLen) break;
+  }
+
+  return (result.slice(0, result.length - 1) + '...');
+}
+
+//solution from the internet
+function cutSentence(line, length) {
+  let words = line.split(' ');
+  let result = '...';
+  for (let i = 1; i < words.length; i++) {
+    let res = words.slice(0, i).join(' ');
+    if (res.length <= length) {
+      result = res + '...';
+    } else {
+      return result;
+    }
+  }
+  return line;
+}
+
+
+/*20170906
+NETWORK ATTACK https://js.checkio.org/mission/network-attack/
+Nicola regularly inspects the local networks for security issues.
+He uses a smart and aggressive program which takes control of computers on the network.
+This program attacks all connected computers simultaneously,
+then uses the captured computers for further attacks.
+Nicola started the virus program in the first computer and
+took note of the time it took to completely capture the network.
+We can help him improve his process by modeling and improving his inspections.
+
+We are given information about the connections in the network and
+the security level for each computer.
+Security level is the time (in minutes) that is required for the virus to capture a machine.
+Capture time is not related to the number of infected computers attacking the machine.
+Infection start from the 0th computer (which is already infected).
+Connections in the network are undirected.
+Security levels are not equal to zero (except infected).
+
+Information about a network is represented as a matrix NxN size,
+where N is a number of computers. If ith computer connected with jth computer,
+then matrix[i][j] == matrix[j][i] == 1, else 0.
+Security levels are placed in the main matrix diagonal,
+so matrix[i][i] is the security level for the ith computer.
+
+You should calculate how much time is required to capture the whole network in minutes.
+
+Input: Network information as a list of lists with integers.
+
+Output: The total time of taken to capture the network as an integer.
+
+Example:
+
+capture([[0, 1, 0, 1, 0, 1],
+         [1, 8, 1, 0, 0, 0],
+         [0, 1, 2, 0, 0, 1],
+         [1, 0, 0, 1, 1, 0],
+         [0, 0, 0, 1, 3, 1],
+         [1, 0, 1, 0, 1, 2]]) == 8
+capture([[0, 1, 0, 1, 0, 1],
+         [1, 1, 1, 0, 0, 0],
+         [0, 1, 2, 0, 0, 1],
+         [1, 0, 0, 1, 1, 0],
+         [0, 0, 0, 1, 3, 1],
+         [1, 0, 1, 0, 1, 2]]) == 4
+capture([[0, 1, 1],
+         [1, 9, 1],
+         [1, 1, 9]]) == 9
+*/
