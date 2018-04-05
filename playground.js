4cfa1ea53eec2d5a9ada961cd9335058e021ed9d
Paraskevas Apostolopoulos <papost@fb.com>
2018-04-04 23:26:17 -0700

    file home.py created

--- playground.js
+++ playground.js
@@ -15,3 +15,29 @@ function addTwoFewTimes(num, counter){
   }
   addTwoFewTimes(num, counter);
 }
+
+
+function hipsterfy(sentence){
+    //create new variable and split the string:
+    var arraySentence = sentence.split(" ");
+//create new variable and use .map() method:
+    var newSentence = arraySentence.map(function(word){
+        var result = "";
+        console.log(word)
+        console.log(word.length);
+
+        var vowels = 'aeiou';
+        for(var i = word.length - 1; i >= 0; i--){
+            console.log("i", i);
+            console.log(vowels.indexOf(word[i]));
+            if(vowels.indexOf(word[i]) !== -1){
+                console.log("in the if for " + i + " " + word[i]);
+                return word.replace("a", "");
+                // break;
+            }
+        }
+        // return word;
+    })
+    return newSentence;
+}
+hipsterfy("runner anaconda");
