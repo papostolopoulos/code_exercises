7582ae17e335fb5a3dc7c471a041c4f2dfdb2ca8
Paraskevas Apostolopoulos <papost@fb.com>
2018-04-05 19:11:58 -0700

    Python House Password exercise in home.py

--- checkio.py/home.py
+++ checkio.py/home.py
@@ -1,1 +1,36 @@
 # coding: utf-8
+
+# 20180405
+# HOUSE PASSWORD https://py.checkio.org/en/mission/house-password/
+# Stephan and Sophia forget about security and use simple passwords for everything.
+# Help Nikola develop a password security check module.
+# The password will be considered strong enough if its length is greater than or
+# equal to 10 symbols,
+# it has at least one digit, as well as containing one uppercase letter and one
+# lowercase letter in it. The password contains only ASCII latin letters or digits.
+#
+# Input: A password as a string.
+# Output: Is the password safe or not as a boolean or any data type that can be
+# converted and processed as a boolean. In the results you will see the converted
+# results.
+#
+# Example:
+# checkio('A1213pokl') == False
+# checkio('bAse730onE') == True
+# checkio('asasasasasasasaas') == False
+# checkio('QWERTYqwerty') == False
+# checkio('123456123456') == False
+# checkio('QwErTy911poqqqq') == True
+
+import re
+
+def house_password(str):
+    return True if len(str) >= 10 and len(re.findall(r'([A-Z])', str)) > 0 and len(re.findall(r'([a-z])', str)) > 0 and len(re.findall(r'([0-9])', str)) > 0 else False
+
+# and len(re.findall(r'([A-Z])', str)) > 0 and len(re.findall(r'([a-z])', str)) > 0 and len(re.findall(r'([0-9])', str)) > 0
+print(house_password('A1213pokl')) # == False
+print(house_password('bAse730onE')) # == True
+print(house_password('asasasasasasasaas')) # == False
+print(house_password('QWERTYqwerty')) # == False
+print(house_password('123456123456')) # == False
+print(house_password('QwErTy911poqqqq')) # == True
--- playground.py
+++ playground.py
@@ -1,51 +1,36 @@
 # coding: utf-8
 
-#20180403
-#DATE AND TIME CONVERTER https://py.checkio.org/en/mission/date-and-time-convertor/
-# Computer date and time format consists only of numbers, for example: 21.05.2018 16:30
-# Humans prefer to see something like this: 21 May 2018 year, 16 hours 30 minutes
-# Your task is simple - convert the input date and time from computer format into a "human" format.
-# Input: Date and time as a string
-# Output: The same date and time, but in a more readable format
+# 20180405
+# HOUSE PASSWORD https://py.checkio.org/en/mission/house-password/
+# Stephan and Sophia forget about security and use simple passwords for everything.
+# Help Nikola develop a password security check module.
+# The password will be considered strong enough if its length is greater than or
+# equal to 10 symbols,
+# it has at least one digit, as well as containing one uppercase letter and one
+# lowercase letter in it. The password contains only ASCII latin letters or digits.
+#
+# Input: A password as a string.
+# Output: Is the password safe or not as a boolean or any data type that can be
+# converted and processed as a boolean. In the results you will see the converted
+# results.
 #
 # Example:
-# date_time("01.01.2000 00:00") == "1 January 2000 year 0 hours 0 minutes"
-# date_time("19.09.2999 01:59") == "19 September 2999 year 1 hour 59 minutes"
-# date_time("21.10.1999 18:01") == "21 October 1999 year 18 hours 1 minute"
-# note: words "hour" and "minute" are used only when time is 01:mm (1 hour) or hh:01 (1 minute).
-# In other cases it should be used "hours" and "minutes".
+# checkio('A1213pokl') == False
+# checkio('bAse730onE') == True
+# checkio('asasasasasasasaas') == False
+# checkio('QWERTYqwerty') == False
+# checkio('123456123456') == False
+# checkio('QwErTy911poqqqq') == True
 
-import datetime
+import re
 
-def date_time(str):
-    hours = "hours"
-    minutes = "minutes"
-    first_list = str.split(".")
-    second_list = first_list[-1].split(" ")
+def house_password(str):
+    return True if len(str) >= 10 and len(re.findall(r'([A-Z])', str)) > 0 and len(re.findall(r'([a-z])', str)) > 0 and len(re.findall(r'([0-9])', str)) > 0 else False
 
-    print(first_list)
-    first_list.remove(first_list[-1])
-    first_list.extend(second_list)
-    second_list = first_list[-1].split(":")
-    first_list.remove(first_list[-1])
-    first_list.extend(second_list)
-
-    for i in range(0, len(first_list)):
-        first_list[i] = int(first_list[i])
-
-    if first_list[3] == 1:
-        hours = "hour"
-
-    if first_list[4] == 1:
-        minutes = "minute"
-
-    return(datetime.datetime(first_list[2], first_list[1], first_list[0], first_list[3], first_list[4]).strftime("%-d %B %Y year %-H {} %-M {}".format(hours, minutes)))
-
-
-
-
-
-
-
-
-print(datetime.datetime(2017, 5, 3, 15, 40).strftime("%-d %B %Y year %-H hours %-M minutes"))
+# and len(re.findall(r'([A-Z])', str)) > 0 and len(re.findall(r'([a-z])', str)) > 0 and len(re.findall(r'([0-9])', str)) > 0
+print(house_password('A1213pokl')) # == False
+print(house_password('bAse730onE')) # == True
+print(house_password('asasasasasasasaas')) # == False
+print(house_password('QWERTYqwerty')) # == False
+print(house_password('123456123456')) # == False
+print(house_password('QwErTy911poqqqq')) # == True
