37b6d208e81c697a6b298ca152bb4b95701e3097
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-04-10 23:28:14 -0700

    safe_pawns checkio for python, partially done

--- playground.py
+++ playground.py
@@ -39,12 +39,17 @@ def safe_pawns(a_set):
     ranks = "12345678"
     counter = 0
     for x in a_set:
-        back_left = columns[columns.find(x[0]) - 1] + str(int(x[1]) - 1)
-        back_right = (columns[columns.find(x[0]) - 1] + str(int(x[1]) + 1))
+        try:
+            back_left = columns[columns.find(x[0]) - 1] + ranks[ranks.find(x[1]) - 1]
+            back_right = columns[columns.find(x[0]) + 1] + ranks[ranks.find(x[1]) - 1]
+        except IndexError:
+            continue;
         if back_left in a_set or back_right in a_set:
             counter += 1
 
     return counter
 
-print(safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"})) # == 6
-print(safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"})) # == 1
+# print(safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"})) # == 6
+# print(safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"})) # == 1
+print(safe_pawns(["a1","b2","c3","d4","e5","f6","g7","h8"]))
+safe_pawns(["a1","a2","a3","a4","h1","h2","h3","h4"])
