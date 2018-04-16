97d69158af2de5c77091129cd28097f4cd4c9bd9
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-04-15 23:02:00 -0700

    clock

--- checkio.py/home.py
+++ checkio.py/home.py
@@ -309,3 +309,40 @@ print(safe_pawns(["a1","a2","a3","a4","h1","h2","h3","h4"])) # == 0
 print(safe_pawns(["a1","b2","c3","d4","e5","f6","g7","h8"])) # == 7
 print(safe_pawns(["a8","b7","c6","d5","e4","f3","g2","h1"])) # == 7
 print(safe_pawns(["a2","b4","c6","d8","e1","f3","g5","h8"])) # == 0
+
+20180413
+In this mission you should write you own py3 implementation
+(but you can use py2 for this) of the built-in functions min and max.
+Some builtin functions are closed here: import, eval, exec, globals.
+Don't forget you should implement two functions in your code.
+
+max(iterable, *[, key]) or min(iterable, *[, key])
+max(arg1, arg2, *args[, key]) or min(arg1, arg2, *args[, key])
+
+Return the largest (smallest) item in an iterable
+or the largest(smallest) of two or more arguments.
+
+If one positional argument is provided, it should be an iterable.
+The largest (smallest) item in the iterable is returned.
+If two or more positional arguments are provided,
+the largest (smallest) of the positional arguments is returned.
+
+The optional keyword-only key argument specifies a function of one argument
+that is used to extract a comparison key from each list element (for example, key=str.lower).
+
+If multiple items are maximal (minimal), the function returns the first one encountered.
+-- Python Documentation (Built-in Functions)
+
+Input: One positional argument as an iterable or two or more positional arguments.
+Optional keyword argument as a function.
+
+Output: The largest item for the "max" function and the smallest for the "min" function.
+
+Example:
+
+max(3, 2) == 3
+min(3, 2) == 2
+max([1, 2, 0, 3, 4]) == 4
+min("hello") == "e"
+max(2.2, 5.6, 5.9, key=int) == 5.6
+min([[1,2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0]
