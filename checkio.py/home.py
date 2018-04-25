a58b324acddc100eba8c14cbd996be4d496b75df
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-04-24 22:16:24 -0700

    ListAll in home.py

--- checkio.py/home.py
+++ checkio.py/home.py
@@ -426,3 +426,25 @@ def long_repeat(str):
 print(long_repeat('sdsffffse')) #== 4
 print(long_repeat('ddvvrwwwrggg')) #== 3
 print(long_repeat("aa")) #== 2
+
+# 20180424
+# ALL THE SAME https://py.checkio.org/en/mission/all-the-same/
+# In this mission you should check if all elements in the given list are equal.
+#
+# Input: List.
+# Output: Bool.
+#
+# Example:
+# all_the_same([1, 1, 1]) == True
+# all_the_same([1, 2, 1]) == False
+# all_the_same(['a', 'a', 'a']) == True
+# all_the_same([]) == True
+
+def all_the_same(lst):
+    return all(lst[0] == lst[i] for i in range(1, len(lst)))
+
+
+print(all_the_same([1, 1, 1])) # == True
+print(all_the_same([1, 2, 1])) # == False
+print(all_the_same(['a', 'a', 'a'])) # == True
+print(all_the_same([])) # == True
--- playground.py
+++ playground.py
@@ -1,44 +1,23 @@
 # coding: utf-8
 
-# 20180423
-# LONG REPEAT https://py.checkio.org/en/mission/long-repeat/
-# There are four substring missions that were born all in one day and
-# you shouldn’t be needed more than one day to solve them.
-# All of those mission can be simply solved by brute force, but is it always
-# the best way to go? (you might not have access to all of those missions yet,
-# but they are going to be available with more opened islands on the map).
+# 20180424
+# ALL THE SAME https://py.checkio.org/en/mission/all-the-same/
+# In this mission you should check if all elements in the given list are equal.
 #
-# This mission is the first one of the series. Here you should find the length
-# of the longest substring that consists of the same letter.
-# For example, line "aaabbcaaaa" contains four substrings with the same letters
-# "aaa", "bb","c" and "aaaa".
-# The last substring is the longest one which makes it an answer.
-#
-# Input: String.
-#
-# Output: Int.
+# Input: List.
+# Output: Bool.
 #
 # Example:
-#
-# long_repeat('sdsffffse') == 4
-# long_repeat('ddvvrwwwrggg') == 3
+# all_the_same([1, 1, 1]) == True
+# all_the_same([1, 2, 1]) == False
+# all_the_same(['a', 'a', 'a']) == True
+# all_the_same([]) == True
 
-def long_repeat(str):
-    if len(str) == 0:
-        return 0
-    result = 1
-    counter = 1
-    for x in range(1, len(str)):
-        if str[x] == str[x - 1]:
-            counter += 1
-            if counter > result:
-                result = counter
-        else:
-            counter = 1
-            
-    return result
+def all_the_same(lst):
+    return all(lst[0] == lst[i] for i in range(1, len(lst)))
 
 
-print(long_repeat('sdsffffse')) #== 4
-print(long_repeat('ddvvrwwwrggg')) #== 3
-print(long_repeat("aa")) #== 2
+print(all_the_same([1, 1, 1])) # == True
+print(all_the_same([1, 2, 1])) # == False
+print(all_the_same(['a', 'a', 'a'])) # == True
+print(all_the_same([])) # == True
